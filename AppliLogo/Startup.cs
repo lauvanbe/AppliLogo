574432f7913646c5c55e325cb294a4ad598effea
lauvanbe <lauvanbe@gmail.com>
2018-05-06 17:17:54 +0200

    Ajoutez des fichiers projet.

---
+++ AppliLogo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AppliLogo.Startup))]
+namespace AppliLogo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
