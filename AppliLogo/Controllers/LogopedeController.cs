4bd8b8f0dc247397b7097511bb690dbd04e839b5
lauvanbe <lauvanbe@gmail.com>
2018-05-06 19:45:51 +0200

    start

---
+++ AppliLogo/Controllers/LogopedeController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+using AppliLogo.Models;
+
+namespace AppliLogo.Controllers
+{
+    public class LogopedeController : Controller
+    {
+        // GET: Logopede
+        public ActionResult VueLogo()
+        {
+            var logo = new Logopede() { NomLogo = "AngeGardien", PrenomLogo = "Josephine" };
+
+            return View(logo);
+        }
+    }
+}
---
+++ AppliLogo/Models/Logopede.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AppliLogo.Models
+{
+    public class Logopede
+    {
+        public int Id { get; set; }
+        public String NomLogo { get; set; }
+        public String PrenomLogo { get; set; }
+
+    }
+}
